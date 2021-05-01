d2784c001f7583340d662480ebfcdc8633db83ef
Robert Iwatt <riwatt@gmail.com>
2021-04-30 23:49:08 -0700

    temp ci: narrow down test cases

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ set -e
 set -u
 set -o pipefail
 
-docker run --rm -it elpy:testing bash -c 'emacs --version ; python --version ; cask --version ; cask install ; PYTHONPATH="`pwd`" cask exec ert-runner --reporter ert+duration'
+docker run --rm -it elpy:testing bash -c 'emacs --version ; python --version ; cask --version ; cask install ; PYTHONPATH="`pwd`" cask exec ert-runner --reporter ert+duration test/elpy-company--cache-name-test.el test/elpy-company-backend-test.el'
